176f907332a82498c9f3f834bb119475f4ad4b76
Aliaksei Patsekhin <45238218+Aleksises@users.noreply.github.com>
2020-02-21 00:21:30 +0300

    Task1,2 (#1)
    * Reverted resource generation
    
    * Task 2
    
    * Refractoring

---
+++ .NET Introduction/NET_Introduction_task2/NET_Introduction_task2/WinFormsApp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using Shared;
+using System;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowGreetings()
+        {
+            if (textBox1.TextLength > 0)
+            {
+                var greetings = GreetingsService.GetGreetingsString(textBox1.Text);
+                MessageBox.Show(greetings, "Greetings", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("You did not provide a username!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void TextBox1_Leave(object sender, EventArgs e)
+        {
+            ShowGreetings();
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                ShowGreetings();
+            }
+        }
+    }
+}
