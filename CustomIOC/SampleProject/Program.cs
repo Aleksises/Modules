aec63c41efaf80e265ea948f169cd9fd2f3a76b1
Aleksises <aleksisess@gmail.com>
2020-03-08 14:19:01 +0300

    Missed IOC example

---
+++ CustomIOC/SampleProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using CustomIOC;
+using SampleProject.SampleClasses;
+using System.Reflection;
+
+namespace SampleProject
+{
+    public class Program
+    {
+        /// <summary>
+        /// The First Example
+        /// </summary>
+        public static void Main()
+        {
+            var container = new CustomIoC();
+            container.AddAssembly(Assembly.GetExecutingAssembly());
+
+            var customerBLL = (CustomerBLL)container.CreateInstance(typeof(CustomerBLL));
+            var customerBLL2 = container.CreateInstance<CustomerBLL>();
+
+            customerBLL.CheckDAL();
+            customerBLL.CheckLogger();
+
+            customerBLL2.CheckLogger();
+            customerBLL2.CheckDAL();
+        }
+
+        /// <summary>
+        /// The Second Example
+        /// </summary>
+        //public static void Main()
+        //{
+        //    var container = new CustomIoC();
+
+        //    container.AddType(typeof(CustomerBLL));
+        //    container.AddType(typeof(Logger));
+        //    container.AddType(typeof(CustomerDAL), typeof(ICustomerDAL));
+
+        //    var customerBLL = (CustomerBLL)container.CreateInstance(typeof(CustomerBLL));
+        //    var customerBLL2 = container.CreateInstance<CustomerBLL>();
+
+        //    customerBLL.CheckDAL();
+        //    customerBLL.CheckLogger();
+
+        //    customerBLL2.CheckLogger();
+        //    customerBLL2.CheckDAL();
+        //}
+    }
+}
---
+++ CustomIOC/SampleProject/SampleClasses/Logger.cs
@@ -0,0 +1,14 @@
+﻿using CustomIOC.Attributes;
+using System;
+
+namespace SampleProject.SampleClasses
+{
+    [Export]
+    public class Logger
+    {
+        public void HealthCheckLogger()
+        {
+            Console.WriteLine($"It's alive {GetType().Name}");
+        }
+    }
+}
