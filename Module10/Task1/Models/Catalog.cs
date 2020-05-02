4aa52f855dae22b326f70578341b9082092c2c96
Aleksises <aleksisess@gmail.com>
2020-05-02 14:58:59 +0300

    Completed Task 1

---
+++ Module10/Task1/Models/Catalog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Task1.Models
+{
+    [XmlRoot(ElementName = "catalog", Namespace = XmlNamespace)]
+    public class Catalog
+    {
+        public const string XmlNamespace = "http://library.by/catalog";
+
+        [XmlAttribute(AttributeName = "date", DataType = "date")]
+        public DateTime Date { get; set; }
+
+        [XmlElement("book")]
+        public List<Book> Books { get; set; }
+    }
+}
---
+++ Module10/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Task1.Models;
+
+namespace Task1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var serializer = new XmlSerializer(typeof(Catalog));
+
+            using var fileStream = File.OpenRead("books.xml");
+            var catalog = (Catalog)serializer.Deserialize(fileStream);
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, Catalog.XmlNamespace);
+
+            var settings = new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                Indent = true
+            };
+            using var xmlWriter = XmlWriter.Create("books1.xml", settings);
+            serializer.Serialize(xmlWriter, catalog, namespaces);
+        }
+    }
+}
